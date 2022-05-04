6143e739bee93445e315d7cdcb1da8da89ebeb3b
Mauricio Gael Hernandez Lom <mhernandezl1605@alumno.ipn.mx>
2022-05-04 18:30:32 -0500

    Paso 8: Cumpliendo el requerimiento de la clase FizzBuzzService con su prueba aprobada

---
+++ services/fizzbuzzservice.js
@@ -0,0 +1,31 @@
+const ExplorerService = require("./explorerservice")
+
+class FizzBuzzService {
+
+    static applyValidationInExplorer(explorers, mission) {
+
+        const dataFilter = ExplorerService.filterByMission(explorers,mission)
+
+        dataFilter.forEach((explorer) => {
+            if (explorer.score % 3 === 0) {
+                explorer.trick = "Fizz"
+            }
+            if(explorer.score % 5 === 0) {
+                explorer.trick = "Buzz"
+            }
+            if(explorer.score % 5 === 0 && explorer.score % 3 === 0) {
+                explorer.trick = "FizzBuzz"
+            }
+
+            if(explorer.score % 5 !== 0 && explorer.score % 3 !== 0) {
+                explorer.trick = explorer.score
+            }
+        })
+
+        return dataFilter
+
+    }
+
+}
+
+module.exports = FizzBuzzService
---
+++ test/fizzbuzzservice.test.js
@@ -0,0 +1,19 @@
+const FizzBuzzService = require("../services/fizzbuzzservice");
+const Reader = require("../services/reader");
+
+
+const data = Reader.readJsonFile('./explorers.json')
+
+describe(('This are the unit test for the FizzBuzzService class'), () => {
+    
+    test(('Test 1: Validate that the function applyValidationInExplorer works'), () => {
+
+        const validatedExplorers = FizzBuzzService.applyValidationInExplorer(data,'node')
+
+        const getTrick = validatedExplorers.map((explorer) => explorer.trick)
+
+        expect(getTrick).toContain('FizzBuzz')
+
+    })
+
+})
